b3774838fee3119ce81e0f1c03f0202c42a37749
Lusca Lima <lucas@siasp.com.br>
2020-07-27 23:08:31 -0300

    Loading local data on page reloading

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,15 @@
 "use strict"
 
 const D = document
+const W = window
 
 let form = null
 let search = null
 let searchPlace = null
 let searchTerm = ""
 let weatherData
+let current = {}
+let weather = {}
 
 /** Get the reference of the form */
 form = D.querySelector("form")
@@ -21,34 +24,26 @@ if (!search) {
   throw new Error("The search field was not found")
 }
 
-let current = {}
-let weather = {}
+/** On reload the page, get the local data of the last search */
+
+W.addEventListener("load", () => {
+  const wacache = localStorage.getItem("wacache")
+  const term = localStorage.getItem("term")
+
+  if (term) {
+    search.setAttribute("value", term)
+  }
+
+  if (wacache) {
+    weatherData = JSON.parse(wacache)
+    main()
+  }
+})
 
 form.addEventListener("submit", async event => {
   event.preventDefault()
-
   weatherData = await getWeather(searchTerm)
-
-  displayContent.style.display = "flex"
-  displayFallback.style.display = "none"
-
-  current = weatherData.forecast.current
-  weather = weatherData.forecast.current.weather[0]
-
-  /* The first value of temp is in Kelvin degrees */
-  current.temp = (((current.temp - 273.15) * 9) / 5 + 32).toFixed(2) // Kelvin to Fahrenheit
-  unityVel(current.wind, suw.value === "ms")
-  unityTemp(current.temp, sut.value === "F")
-
-  displayHeader.textContent = weatherData.location.text
-  ddwind.textContent = current.wind_speed
-  ddmois.textContent = current.humidity
-  ddtemp.textContent = current.temp
-
-  ddimg.setAttribute("src", `/img/temp/${weather.icon}@4x.png`)
-  ddweat.textContent = weather.main
-  ddweatd.textContent = weather.description
-  ddfooter.textContent = weatherData.location.place_name
+  main()
 })
 
 search.addEventListener("input", ({ target }) => {
@@ -68,7 +63,7 @@ async function getWeather(location) {
   const parsedData = await data.json()
 
   localStorage.setItem("term", searchTerm)
-  localStorage.setItem("wacache", parsedData)
+  localStorage.setItem("wacache", JSON.stringify(parsedData))
 
   return parsedData
 }
@@ -105,4 +100,29 @@ function kmhToMs(vel, pass = false) {
   if (pass) return
 
   current.wind_speed = (vel / 3.6).toFixed(2)
+}
+
+/** Main functions */
+
+function main() {
+  displayContent.style.display = "flex"
+  displayFallback.style.display = "none"
+
+  current = weatherData.forecast.current
+  weather = weatherData.forecast.current.weather[0]
+
+  /* The first value of temp is in Kelvin degrees */
+  current.temp = (((current.temp - 273.15) * 9) / 5 + 32).toFixed(2) // Kelvin to Fahrenheit
+  unityVel(current.wind, suw.value === "ms")
+  unityTemp(current.temp, sut.value === "F")
+
+  displayHeader.textContent = weatherData.location.text
+  ddwind.textContent = current.wind_speed
+  ddmois.textContent = current.humidity
+  ddtemp.textContent = current.temp
+
+  ddimg.setAttribute("src", `/img/temp/${weather.icon}@4x.png`)
+  ddweat.textContent = weather.main
+  ddweatd.textContent = weather.description
+  ddfooter.textContent = weatherData.location.place_name
 }
